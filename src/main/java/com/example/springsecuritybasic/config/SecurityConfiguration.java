f8ebb53b32be1e88bc88da4eb8df3acd39042ceb
Duyen Pham <duyen_pham@epam.com>
2022-05-22 22:35:27 +0700

    add permitAll and denyAll

--- src/main/java/com/example/springsecuritybasic/config/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritybasic/config/SecurityConfiguration.java
@@ -17,7 +17,30 @@ public class SecurityConfiguration {
                 .antMatchers("/secured").authenticated()
                 .antMatchers("/not-secured").permitAll()
                 .and()
+                .formLogin()
+                .and()
                 .httpBasic(withDefaults());
+
+        /*Permit all */
+        /*
+        http
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic(withDefaults());
+        */
+
+        /*Deny all, 403 returns even with username and password */
+        /*
+        http
+                .authorizeHttpRequests()
+                .anyRequest().denyAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic(withDefaults());*/
         return http.build();
     }
 
